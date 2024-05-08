a97c73daeafe8eabcd58099e7ccd227b4025d795
HideOnChimes <pedrogontijo75@gmail.com>
2024-05-08 13:40:18 -0300

    arrumando os erros com o git

--- Assets/Scripts/Mobile/TouchController.cs
+++ Assets/Scripts/Mobile/TouchController.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 public class TouchController : MonoBehaviour
 {
     public FixedTouchField _FixedTouchField;
-    public CameraMove _Camera;
-    void Start()
-    {
-        
-    }
-
-    
+    public PlayerCam _Camera;
     void Update()
     {
         _Camera.LockAxis = _FixedTouchField.TouchDist;
